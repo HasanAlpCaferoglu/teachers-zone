42c4e02f8b86e1a51057dccc6c8637b82d60dcbe
HasanAlpCaferoglu <alp.caferoglu@gmail.com>
2022-07-02 14:32:15 +0300

    style changed for telephone

--- components/Header.js
+++ components/Header.js
@@ -12,9 +12,9 @@ function Header() {
 
   return (
     <div className="mt-0 flex items-center justify-between h-auto bg-[#0b4961]">
-      <div className="cursor-pointer flex flex-col m-3 font-Anton text-2xl text-[#c4c6c9] xl:text-4xl xl:flex-row xl:m-5">
+      <div className="cursor-pointer flex flex-col m-3 font-Anton text-xl text-[#c4c6c9] md:2xl lg:text-4xl lg:flex-row lg:m-5">
         <Link href="/">TEACHERS</Link>
-        <p href="/" className="hidden xl:block">
+        <p href="/" className="hidden lg:block">
           -
         </p>
         <Link href="/">ZONE</Link>
@@ -27,11 +27,11 @@ function Header() {
             router.push(`/?searchForm=${e.target.value}`);
           }} // set the value of state searchQuery anytime the user types in the searchbox
           placeholder="Search for..."
-          className="rounded-full px-2 xl:pr-10 xl:py-1"
+          className="rounded-full text-xs p-1 w-24 sm:text-base sm:w-32 md:w-40 lg:w-56 xl:pr-10 xl:py-1"
         />
-        <SearchIcon className="h-4 ml-2 text-[#c4c6c9] xl:h-7" />
+        <SearchIcon className="h-5 ml-2 text-[#c4c6c9] xl:h-7" />
       </form>
-      <div className="cursor-pointer h-full text-md mr-5 bg-orange-500 rounded-full px-2 py-1 text-white font-bold transition duration-100 transform hover:animate-bounce xl:px-8 xl:text-xl">
+      <div className="cursor-pointer h-full text-xs mr-5 bg-orange-500 rounded-full px-2 py-1 text-white font-bold transition duration-100 transform hover:animate-bounce sm:text-base xl:px-8 xl:text-lg">
         <button onClick={() => router.push("/add-teacher")}>
           I am a teacher
         </button>
